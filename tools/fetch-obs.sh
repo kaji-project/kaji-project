133c6c59883f04360f3834d56f54783fccf68be0
Sebastien Coavoux <sebastien.coavoux@savoirfairelinux.com>
2014-12-09 14:32:56 -0500

    Fix path

--- tools/fetch-obs.sh
+++ tools/fetch-obs.sh
@@ -8,12 +8,12 @@ mkdir -p $BASEDIR
 # Will download into $BASEDIR/$DISTRO
 wget -r  --no-parent -P $BASEDIR -nH  --cut-dirs=3 http://download.opensuse.org/repositories/home:/kaji-project/$DISTRO
 
-find $BASEDIR -name '*.html*' -exec rm {} \;
-find $BASEDIR -name '*.btih' -exec rm {} \;
-find $BASEDIR -name '*.magnet' -exec rm {} \;
-find $BASEDIR -name '*.md5' -exec rm {} \;
-find $BASEDIR -name '*.meta4' -exec rm {} \;
-find $BASEDIR -name '*.metalink' -exec rm {} \;
-find $BASEDIR -name '*.mirrorlist' -exec rm {} \;
-find $BASEDIR -name '*.sha1' -exec rm {} \;
-find $BASEDIR -name '*.sha256' -exec rm {} \;
+find $BASEDIR/$DISTRO -name '*.html*' -exec rm {} \;
+find $BASEDIR/$DISTRO -name '*.btih' -exec rm {} \;
+find $BASEDIR/$DISTRO -name '*.magnet' -exec rm {} \;
+find $BASEDIR/$DISTRO -name '*.md5' -exec rm {} \;
+find $BASEDIR/$DISTRO -name '*.meta4' -exec rm {} \;
+find $BASEDIR/$DISTRO -name '*.metalink' -exec rm {} \;
+find $BASEDIR/$DISTRO -name '*.mirrorlist' -exec rm {} \;
+find $BASEDIR/$DISTRO -name '*.sha1' -exec rm {} \;
+find $BASEDIR/$DISTRO -name '*.sha256' -exec rm {} \;
