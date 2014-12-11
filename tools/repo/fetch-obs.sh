30f39d8f98f802823ba611d513f27d1d02a348ed
Sebastien Coavoux <sebastien.coavoux@savoirfairelinux.com>
2014-12-11 13:52:25 -0500

    Fix : Path in fetch-obs

--- tools/repo/fetch-obs.sh
+++ tools/repo/fetch-obs.sh
@@ -7,14 +7,14 @@ DISTRO=$3
 mkdir -p $BASEDIR
 
 # Will download into $BASEDIR/$DISTRO
-wget -r  --no-parent -P $BASEDIR -nH  --cut-dirs=3 http://download.opensuse.org/repositories/home:/$URL/$DISTRO
+wget -r  --no-parent -P $BASEDIR -nH  --cut-dirs=2 http://download.opensuse.org/repositories/home:/$URL/$DISTRO
 
-find $BASEDIR/$DISTRO -name '*.html*' -exec rm {} \;
-find $BASEDIR/$DISTRO -name '*.btih' -exec rm {} \;
-find $BASEDIR/$DISTRO -name '*.magnet' -exec rm {} \;
-find $BASEDIR/$DISTRO -name '*.md5' -exec rm {} \;
-find $BASEDIR/$DISTRO -name '*.meta4' -exec rm {} \;
-find $BASEDIR/$DISTRO -name '*.metalink' -exec rm {} \;
-find $BASEDIR/$DISTRO -name '*.mirrorlist' -exec rm {} \;
-find $BASEDIR/$DISTRO -name '*.sha1' -exec rm {} \;
-find $BASEDIR/$DISTRO -name '*.sha256' -exec rm {} \;
+find $BASEDIR/$URL/$DISTRO -name '*.html*' -exec rm {} \;
+find $BASEDIR/$URL/$DISTRO -name '*.btih' -exec rm {} \;
+find $BASEDIR/$URL/$DISTRO -name '*.magnet' -exec rm {} \;
+find $BASEDIR/$URL/$DISTRO -name '*.md5' -exec rm {} \;
+find $BASEDIR/$URL/$DISTRO -name '*.meta4' -exec rm {} \;
+find $BASEDIR/$URL/$DISTRO -name '*.metalink' -exec rm {} \;
+find $BASEDIR/$URL/$DISTRO -name '*.mirrorlist' -exec rm {} \;
+find $BASEDIR/$URL/$DISTRO -name '*.sha1' -exec rm {} \;
+find $BASEDIR/$URL/$DISTRO -name '*.sha256' -exec rm {} \;
